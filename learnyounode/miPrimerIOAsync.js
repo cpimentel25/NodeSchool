1e0d56579ec78ef569646a377d5b9a054a90aaf6
CP <cpimentel.0325@gmail.com>
2022-10-20 16:27:07 -0500

    Four exercise update v2.2

--- learnyounode/miPrimerIOAsync.js
+++ learnyounode/miPrimerIOAsync.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function(err, data){
-    console.log(data.toString().split('\n').length -1);
+    if (err) {
+        console.log(err);
+    }
+    const result = data.toString().split('\n').length -1;
+    console.log(result);
 });
