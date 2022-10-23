34e21490e6f303b01c886aac3ecd414befbd1e5c
CP <cpimentel.0325@gmail.com>
2022-10-23 18:12:02 -0500

    07 Schoolnode

---
+++ learnyounode/07_ClienteHttp.js
@@ -0,0 +1,43 @@
+const http = require("http");
+
+// process.argv.forEach((val, index) => { // <- Imprimimos los valores de "process.argv".
+//     console.log(`${index}: ${val}`);
+// });
+
+const URL = process.argv[2];
+
+//Metodo 1 http.get() (node v12):
+let res = http.get(URL, callback);
+
+res.on("error", (err) => { // <- Si existe un error, nos arroja en consola el error.
+  console.error(`Got error: ${err.message}`);
+});
+
+function callback(response) {
+  response.setEncoding("utf8");
+  //   console.log(response.setEncoding("utf8"));
+
+  response.on("data", (chunk) => {
+    console.log(chunk);
+  });
+  response.on("end", () => {
+    try {
+      const parsedData = JSON.parse(rawData);
+      console.log(parsedData);
+    } catch (error) {
+      console.error(`Got error: ${error.message}`);
+    }
+  });
+}
+
+//Metodo 3 (node v19.0+):
+// function httpGet() {
+//     const req = new XMLHttpRequest();
+//     req.open('GET', process.argv);
+//     req.send();
+//     // req.onload = () => console.log(req.responseText)
+
+//     // console.log(req.responseText);
+
+//     return console.log(req.responseText);
+// }
